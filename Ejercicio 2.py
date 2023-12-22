44f5fc6783793dcea78b63d7def6f78c06dae091
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-12-22 09:57:15 +0100

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,24 +1,17 @@
 corredor = {}
+q = 1   
 
-q = 1
 while q == 1:
     try:
-   
         hora = input("a que hora ha pasado")
         km = int(input("por que km va, introducir fin si acabo la carrera"))
-
-    
-
         corredor[hora] = km
 
-
-
     except ValueError:
-
         for i in corredor:
             x = corredor[i]
-            print("A las", i, "estaba en el km", x)
-        
-        q = q + 1
+            print("A las", i, "estaba en el km", x)  
+            q = q + 1  
+
 
     
